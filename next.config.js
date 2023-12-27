0d0da483668ff93e0ec89eba848b746729cafedb
Inder-Pal-github <inder39811@gmail.com>
2023-12-28 01:03:56 +0530

    setup: gh pages

--- next.config.js
+++ next.config.js
@@ -2,5 +2,21 @@
 const nextConfig = {
     output:"export"
 }
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-module.exports = nextConfig
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    nextConfig
+  }
