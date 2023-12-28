1800c73b8074cebd83c9d79e6f3833bb9a4a8907
Pyare Lal <114997244+pyarelalchauhan@users.noreply.github.com>
2023-12-28 08:03:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,6 @@
 const nextConfig = {
     output:"export"
 }
-const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-    nextConfig
-  }
+module.exports = nextConfig
+  
