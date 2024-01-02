e4fdb001accaae08f747ebe6deb14550ff9696b5
inder231 <inder39811@gmail.com>
2024-01-02 15:42:28 +0530

    feat: added components to main file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import About from "./components/about/page";
+import Contact from "./components/contact/page";
+import Experience from "./components/experience/page";
+import Intro from "./components/intro/page";
+import Projects from "./components/projects/page";
+import Skills from "./components/skills/page";
 
 export default function Home() {
   return (
     <main className="relative dark:text-white h-full">
-      <h1>Main page</h1>
+      <Intro />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
   );
 }
