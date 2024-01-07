f3d213e43202cb4bd45f53ba15e7b3f5f600ace2
inder231 <inder39811@gmail.com>
2024-01-07 11:44:25 +0530

    ref: css modification and removed content

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -12,37 +12,33 @@ export default function Intro() {
   return (
     <div
       id="intro"
-      className="min-h-[100vh] p-[6rem] w-full flex flex-col-reverse md:flex-row gap-8 items-center justify-between border-2"
+      className="min-h-[100vh] p-[8rem] w-full flex flex-col-reverse md:flex-row gap-8 items-center justify-between border-2"
     >
       <div
-        className="flex-2 text-sm md:text-lg text-center p-6 italic"
-        data-aos="zoom-out-right"
+        className="flex-2 text-zinc-600 text-sm md:text-lg text-center p-6 italic selection:text-teal-500"
+        data-aos="fade-up"
+        data-aos-anchor-placement="bottom-bottom"
       >
         I&apos;m <span className="text-[2rem] font-bold">Pyare Lal,</span> a
         Ph.D. Scholar at the{" "}
         <b>
-          Indian Institute of Science Education and Research, Bhopal (IISERB)
+          Indian Institute of Science Education and Research, Bhopal (IISERB).
         </b>
-        , where I am pursuing my doctoral studies in the{" "}
-        <b>Department of Data Science and Engineering (DSE)</b> within the GeoAI
-        Lab. Under the guidance of Dr. Vaibhav Kumar, I delve into the exciting
-        realms of <b>Machine Learning and Deep Learning</b>, specializing in the
-        field of <b>GeoAI</b>.
       </div>
-      <div className="flex-3 relative" data-aos="zoom-out">
+      <div className="flex-3 flex flex-col justify-center items-center relative" data-aos="zoom-out">
         <div
           style={{
             clipPath:
               "polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)",
           }}
-          className="relative h-[12rem] w-[12rem] bg-gradient-to-r from-cyan-500 to-blue-500"
+          className="relative h-[14rem] w-[14rem] bg-gradient-to-r from-cyan-500 to-blue-500"
         >
           <Image
             src="/images/profile.png"
             alt="profile-background"
             width={400}
             height={400}
-            className="absolute top-[10%] left-[50%] -translate-x-1/2"
+            className="absolute top-[15%] left-[52%] -translate-x-1/2"
           />
         </div>
         <div className="flex flex-row items-center justify-between m-4">
@@ -51,24 +47,27 @@ export default function Intro() {
             aria-label="GitHub"
             target="_blank"
             rel="noopener noreferrer"
+            className="m-4 p-4 border border-zinc-200 shadow-md rounded-md"
           >
-            <FaGithub size="1.5rem" />
+            <FaGithub size="2rem" />
           </a>
           <a
             href="mailto:pyare22@iiserb.ac.in"
             aria-label="Mail"
             target="_blank"
             rel="noopener noreferrer"
+            className="m-4 p-4 border border-zinc-200 shadow-md rounded-md"
           >
-            <MdAttachEmail size="1.5rem" />
+            <MdAttachEmail size="2rem" />
           </a>
           <a
             href="https://www.linkedin.com/in/pyarelaldse/"
             aria-label="LinkedIn"
             target="_blank"
             rel="noopener noreferrer"
+            className="m-4 p-4 border border-zinc-200 shadow-md rounded-md"
           >
-            <FaLinkedin size="1.5rem" />
+            <FaLinkedin size="2rem" />
           </a>
         </div>
       </div>
