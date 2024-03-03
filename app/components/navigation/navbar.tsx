c050f10f16711d26cd07b2191038d3e2b9da14c1
Inder Pal <101566674+inder231@users.noreply.github.com>
2024-03-03 16:59:43 +0530

    button padding fix

--- app/components/navigation/navbar.tsx
+++ app/components/navigation/navbar.tsx
@@ -134,7 +134,7 @@ const Navbar = (props: Props) => {
             </ul>
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mx-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:px-4 sm:py-2 p-2 mx-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <a
                 href="https://drive.google.com/file/d/1P3tfaoqQnkW3193KEUn4X0WNQsSQwvUx/view"
