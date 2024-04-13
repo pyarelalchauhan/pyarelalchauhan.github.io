7febc750f2e7b172fa222f91331a0568a84e067e
Pyare Lal <114997244+pyarelalchauhan@users.noreply.github.com>
2024-04-13 10:14:06 +0530

    Update intro.tsx

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -5,9 +5,7 @@ import pyareProfile from "@/public/images/Pyare-profile-1.png";
 
 export default function Intro() {
   return (
-    <div id="intro"
-      className="px-[1rem] py-[5rem] sm:p-[5rem] mt-[4rem]"
-    >
+    <div id="intro" className="px-[1rem] py-[5rem] sm:p-[5rem] mt-[4rem]">
       <div className="min-h-[70dvh] bg-gray-100 dark:bg-transparent flex flex-col-reverse md:flex-row gap-2 items-center justify-evenly">
         <div className="sm:flex-2 flex-2 text-zinc-700 dark:text-zinc-400 text-sm md:text-lg text-center p-6 italic selection:text-teal-500">
           <div>
@@ -15,14 +13,26 @@ export default function Intro() {
             <span className="md:text-[3rem] text-[2rem] font-bold font-serif">
               Pyare Lal,
             </span>{" "}
-            a Ph.D. Scholar at the{" "}
+            currently pursuing my Ph.D. at the{" "}
             <b>
-              Indian Institute of Science Education and Research, Bhopal
-              (IISERB).
-            </b>
+              <a href="https://www.iiserb.ac.in/" target="_blank" rel="noopener noreferrer">
+                Indian Institute of Science Education and Research, Bhopal (IISERB)
+              </a>
+            </b>{" "}
+            in the{" "}
+            <b>
+              <a href="https://dse.iiserb.ac.in/" target="_blank" rel="noopener noreferrer">
+                Data Science and Engineering Department
+              </a>
+            </b>{" "}
+            within the GeoAI4Cities Lab under the supervision of{" "}
+            <a href="https://sites.google.com/view/vaibhavkumar1/home" target="_blank" rel="noopener noreferrer">
+              Dr. Vaibhav Kumar
+            </a>
+            . My research focuses on utilizing 3D Vision, LiDAR, Deep Learning, and geospatial data.
           </div>
         </div>
-        <div className=" sm:flex-3  flex-3 flex flex-col justify-center items-center relative">
+        <div className="sm:flex-3 flex-3 flex flex-col justify-center items-center relative">
           <div className="animate-float relative h-[14rem] w-[14rem]">
             <Image
               src={pyareProfile}
