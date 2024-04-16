f25b039ea0c7d6dc5bb028516b66075958e9449d
Pyare Lal <114997244+pyarelalchauhan@users.noreply.github.com>
2024-04-16 16:36:34 +0530

    Update intro.tsx

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdAttachEmail } from "react-icons/md";
-import GoogleScholarIcon from "@/public/images/gs.svg"; // Import Google Scholar icon
 import pyareProfile from "@/public/images/Pyare-Profile-2.png";
 
 export default function Intro() {
@@ -39,7 +38,7 @@ export default function Intro() {
           </div>
         </div>
         <div className="sm:flex-3 flex-3 flex flex-col justify-center items-center relative">
-          <div className="h-[14rem] w-[14rem] mt-[-3rem]"> {/* Adjust the margin top to move the image up */}
+          <div className="h-[14rem] w-[14rem]">
             <Image
               src={pyareProfile}
               alt="profile-background"
@@ -89,20 +88,6 @@ export default function Intro() {
                 <FaLinkedin size="1.5rem" />
                 <span className="absolute opacity-0 group-hover:opacity-100 group-hover:text-gray-700 group-hover:text-sm group-hover:-translate-y-10 duration-700">
                   LinkedIn
-                </span>
-              </a>
-            </section>
-            <section className="flex justify-center items-center">
-              <a
-                href="https://scholar.google.com/citations?user=ybKOBzkAAAAJ&hl=en&authuser=1"
-                aria-label="Google Scholar"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="m-4 group flex justify-center p-2 rounded-md drop-shadow-xl bg-gradient-to-r from-gray-800 to-black text-white font-semibold hover:translate-y-3 hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]"
-              >
-                <GoogleScholarIcon width="24" height="24" /> {/* Display Google Scholar SVG icon */}
-                <span className="absolute opacity-0 group-hover:opacity-100 group-hover:text-gray-700 group-hover:text-sm group-hover:-translate-y-10 duration-700">
-                  Google Scholar
                 </span>
               </a>
             </section>
